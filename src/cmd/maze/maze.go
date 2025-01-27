d2cbd1a03bdf8359f2dd38e8123f3e3774881e82
Calamarp <calamarp@student.21-school.ru>
2025-01-27 16:50:19 +0300

    Она рабоатет

--- src/cmd/maze/maze.go
+++ src/cmd/maze/maze.go
@@ -313,3 +313,209 @@
 // // 		log.Fatal(err)
 // // 	}
 // // }
+
+/*********************************************/
+
+// func (g *Game) LoadMazeFromFile(filename string) error {
+// 	file, err := os.Open(filename)
+// 	if err != nil {
+// 		return fmt.Errorf("ошибка открытия файла: %v", err)
+// 	}
+// 	defer file.Close()
+
+// 	scanner := bufio.NewScanner(file)
+
+// 	// Считываем первую строку с размерами лабиринта.
+// 	if !scanner.Scan() {
+// 		return fmt.Errorf("файл пуст или недоступен")
+// 	}
+// 	dimensions := strings.Fields(scanner.Text())
+// 	fmt.Println("Считанные размеры лабиринта:", dimensions)
+// 	if len(dimensions) != 2 {
+// 		log.Fatal("Неверный формат файла: первая строка должна содержать размеры пещеры.")
+// 	}
+
+// 	width, err := strconv.Atoi(dimensions[0])
+// 	if err != nil || width <= 0 || width > maxMazeSize {
+// 		log.Fatal("Неверная ширина пещеры:", err)
+// 	}
+
+// 	height, err := strconv.Atoi(dimensions[1])
+// 	if err != nil || height <= 0 || height > maxMazeSize {
+// 		log.Fatal("Неверная высота пещеры:", err)
+// 	}
+
+// 	g.maze = NewMaze(height, width)
+// 	fmt.Printf("Создан лабиринт размером %dx%d\n", width, height)
+
+// 	// Считываем матрицу правых стенок.
+// 	fmt.Println("Матрица со стенками справа:")
+// 	for y := 0; y < height; y++ {
+// 		if !scanner.Scan() {
+// 			return fmt.Errorf("недостаточно строк для матрицы правых стенок")
+// 		}
+// 		row := strings.Fields(scanner.Text())
+// 		fmt.Printf("Считанная строка %d: %v\n", y+1, row)
+// 		if len(row) != width {
+// 			log.Fatal("Неверный формат файла: количество столбцов не совпадает с заданной шириной.")
+// 		}
+// 		for x, cell := range row {
+// 			if cell == "0" {
+// 				g.maze.Cells[y][x].Right = false
+// 			} else if cell == "1" {
+// 				g.maze.Cells[y][x].Right = true
+// 			} else {
+// 				log.Fatal("Неверный символ в пещере: должен быть 0 или 1.")
+// 			}
+// 			fmt.Printf("%s ", cell)
+// 		}
+// 		fmt.Println()
+// 	}
+
+// 	// Пропускаем пустую строку между матрицами.
+// 	if !scanner.Scan() || scanner.Text() != "" {
+// 		log.Fatal("Ожидалась пустая строка между матрицами.")
+// 	}
+// 	fmt.Println("Пустая строка между матрицами успешно пропущена.")
+
+// 	// Считываем матрицу нижних стенок.
+// 	fmt.Println("\nМатрица со стенками снизу:")
+// 	for y := 0; y < height; y++ {
+// 		if !scanner.Scan() {
+// 			return fmt.Errorf("недостаточно строк для матрицы нижних стенок")
+// 		}
+// 		row := strings.Fields(scanner.Text())
+// 		fmt.Printf("Считанная строка %d: %v\n", y+1, row)
+// 		if len(row) != width {
+// 			log.Fatal("Неверный формат файла: количество столбцов не совпадает с заданной шириной.")
+// 		}
+// 		for x, cell := range row {
+// 			if cell == "0" {
+// 				g.maze.Cells[y][x].Bottom = false
+// 			} else if cell == "1" {
+// 				g.maze.Cells[y][x].Bottom = true
+// 			} else {
+// 				log.Fatal("Неверный символ в пещере: должен быть 0 или 1.")
+// 			}
+// 			fmt.Printf("%s ", cell)
+// 		}
+// 		fmt.Println()
+// 	}
+
+// 	return nil
+// }
+
+// LoadMaze загружает лабиринт из файла
+// func LoadMaze(filename string) (*Maze, error) {
+// 	file, err := os.Open(filename)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer file.Close()
+
+// 	var rows, cols int
+// 	_, err = fmt.Fscanf(file, "%d %d\n", &rows, &cols)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	maze := &Maze{
+// 		Rows:  rows,
+// 		Cols:  cols,
+// 		Cells: make([][]Cell, rows),
+// 	}
+
+// 	for i := range maze.Cells {
+// 		maze.Cells[i] = make([]Cell, cols)
+// 	}
+
+// 	// Читаем первую матрицу (стенки справа)
+// 	for y := 0; y < rows; y++ {
+// 		for x := 0; x < cols; x++ {
+// 			var wall int
+// 			_, err = fmt.Fscanf(file, "%d", &wall)
+// 			if err != nil {
+// 				return nil, err
+// 			}
+// 			if wall == 1 {
+// 				maze.Cells[y][x].Right = true
+// 			}
+// 		}
+// 	}
+
+// 	// Читаем вторую матрицу (стенки снизу)
+// 	for y := 0; y < rows; y++ {
+// 		for x := 0; x < cols; x++ {
+// 			var wall int
+// 			_, err = fmt.Fscanf(file, "%d", &wall)
+// 			if err != nil {
+// 				return nil, err
+// 			}
+// 			if wall == 1 {
+// 				maze.Cells[y][x].Bottom = true
+// 			}
+// 		}
+// 	}
+
+// 	return maze, nil
+// }
+
+// func (m *Maze) GeneratePerfectMaze() {
+// 	type Edge struct {
+// 		x1, y1, x2, y2 int // Координаты соединяемых клеток
+// 	}
+
+// 	edges := []Edge{}
+// 	for y := 0; y < m.Rows; y++ {
+// 		for x := 0; x < m.Cols; x++ {
+// 			if x < m.Cols-1 {
+// 				edges = append(edges, Edge{x, y, x + 1, y}) // Правое соединение
+// 			}
+// 			if y < m.Rows-1 {
+// 				edges = append(edges, Edge{x, y, x, y + 1}) // Нижнее соединение
+// 			}
+// 		}
+// 	}
+
+// 	// Перемешиваем ребра
+// 	rand.Shuffle(len(edges), func(i, j int) {
+// 		edges[i], edges[j] = edges[j], edges[i]
+// 	})
+
+// 	// Создаем структуру для отслеживания соединенных компонентов
+// 	parent := make([]int, m.Rows*m.Cols)
+// 	for i := range parent {
+// 		parent[i] = i
+// 	}
+
+// 	find := func(x int) int {
+// 		if parent[x] != x {
+// 			parent[x] = find(parent[x])
+// 		}
+// 		return parent[x]
+// 	}
+
+// 	union := func(x, y int) {
+// 		rootX := find(x)
+// 		rootY := find(y)
+// 		if rootX != rootY {
+// 			parent[rootX] = rootY
+// 		}
+// 	}
+
+// 	// Генерация лабиринта
+// 	for _, edge := range edges {
+// 		cell1 := edge.y1*m.Cols + edge.x1
+// 		cell2 := edge.y2*m.Cols + edge.x2
+
+// 		if find(cell1) != find(cell2) {
+// 			// Если клетки не соединены, соединяем их и убираем стену
+// 			union(cell1, cell2)
+// 			if edge.x1 == edge.x2 { // Если соединение вертикальное
+// 				m.Cells[edge.y1][edge.x1].Bottom = false
+// 			} else { // Если соединение горизонтальное
+// 				m.Cells[edge.y1][edge.x1].Right = false
+// 			}
+// 		}
+// 	}
+// }
--- src/cmd/maze/maze1.go
+++ src/cmd/maze/maze1.go
@@ -192,150 +192,6 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return mazeWidth, mazeHeight + buttonHeight // Общая высота с кнопкой
 }
 
-// func (g *Game) LoadMazeFromFile(filename string) error {
-// 	file, err := os.Open(filename)
-// 	if err != nil {
-// 		return fmt.Errorf("ошибка открытия файла: %v", err)
-// 	}
-// 	defer file.Close()
-
-// 	scanner := bufio.NewScanner(file)
-
-// 	// Считываем первую строку с размерами лабиринта.
-// 	if !scanner.Scan() {
-// 		return fmt.Errorf("файл пуст или недоступен")
-// 	}
-// 	dimensions := strings.Fields(scanner.Text())
-// 	fmt.Println("Считанные размеры лабиринта:", dimensions)
-// 	if len(dimensions) != 2 {
-// 		log.Fatal("Неверный формат файла: первая строка должна содержать размеры пещеры.")
-// 	}
-
-// 	width, err := strconv.Atoi(dimensions[0])
-// 	if err != nil || width <= 0 || width > maxMazeSize {
-// 		log.Fatal("Неверная ширина пещеры:", err)
-// 	}
-
-// 	height, err := strconv.Atoi(dimensions[1])
-// 	if err != nil || height <= 0 || height > maxMazeSize {
-// 		log.Fatal("Неверная высота пещеры:", err)
-// 	}
-
-// 	g.maze = NewMaze(height, width)
-// 	fmt.Printf("Создан лабиринт размером %dx%d\n", width, height)
-
-// 	// Считываем матрицу правых стенок.
-// 	fmt.Println("Матрица со стенками справа:")
-// 	for y := 0; y < height; y++ {
-// 		if !scanner.Scan() {
-// 			return fmt.Errorf("недостаточно строк для матрицы правых стенок")
-// 		}
-// 		row := strings.Fields(scanner.Text())
-// 		fmt.Printf("Считанная строка %d: %v\n", y+1, row)
-// 		if len(row) != width {
-// 			log.Fatal("Неверный формат файла: количество столбцов не совпадает с заданной шириной.")
-// 		}
-// 		for x, cell := range row {
-// 			if cell == "0" {
-// 				g.maze.Cells[y][x].Right = false
-// 			} else if cell == "1" {
-// 				g.maze.Cells[y][x].Right = true
-// 			} else {
-// 				log.Fatal("Неверный символ в пещере: должен быть 0 или 1.")
-// 			}
-// 			fmt.Printf("%s ", cell)
-// 		}
-// 		fmt.Println()
-// 	}
-
-// 	// Пропускаем пустую строку между матрицами.
-// 	if !scanner.Scan() || scanner.Text() != "" {
-// 		log.Fatal("Ожидалась пустая строка между матрицами.")
-// 	}
-// 	fmt.Println("Пустая строка между матрицами успешно пропущена.")
-
-// 	// Считываем матрицу нижних стенок.
-// 	fmt.Println("\nМатрица со стенками снизу:")
-// 	for y := 0; y < height; y++ {
-// 		if !scanner.Scan() {
-// 			return fmt.Errorf("недостаточно строк для матрицы нижних стенок")
-// 		}
-// 		row := strings.Fields(scanner.Text())
-// 		fmt.Printf("Считанная строка %d: %v\n", y+1, row)
-// 		if len(row) != width {
-// 			log.Fatal("Неверный формат файла: количество столбцов не совпадает с заданной шириной.")
-// 		}
-// 		for x, cell := range row {
-// 			if cell == "0" {
-// 				g.maze.Cells[y][x].Bottom = false
-// 			} else if cell == "1" {
-// 				g.maze.Cells[y][x].Bottom = true
-// 			} else {
-// 				log.Fatal("Неверный символ в пещере: должен быть 0 или 1.")
-// 			}
-// 			fmt.Printf("%s ", cell)
-// 		}
-// 		fmt.Println()
-// 	}
-
-// 	return nil
-// }
-
-// LoadMaze загружает лабиринт из файла
-// func LoadMaze(filename string) (*Maze, error) {
-// 	file, err := os.Open(filename)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer file.Close()
-
-// 	var rows, cols int
-// 	_, err = fmt.Fscanf(file, "%d %d\n", &rows, &cols)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	maze := &Maze{
-// 		Rows:  rows,
-// 		Cols:  cols,
-// 		Cells: make([][]Cell, rows),
-// 	}
-
-// 	for i := range maze.Cells {
-// 		maze.Cells[i] = make([]Cell, cols)
-// 	}
-
-// 	// Читаем первую матрицу (стенки справа)
-// 	for y := 0; y < rows; y++ {
-// 		for x := 0; x < cols; x++ {
-// 			var wall int
-// 			_, err = fmt.Fscanf(file, "%d", &wall)
-// 			if err != nil {
-// 				return nil, err
-// 			}
-// 			if wall == 1 {
-// 				maze.Cells[y][x].Right = true
-// 			}
-// 		}
-// 	}
-
-// 	// Читаем вторую матрицу (стенки снизу)
-// 	for y := 0; y < rows; y++ {
-// 		for x := 0; x < cols; x++ {
-// 			var wall int
-// 			_, err = fmt.Fscanf(file, "%d", &wall)
-// 			if err != nil {
-// 				return nil, err
-// 			}
-// 			if wall == 1 {
-// 				maze.Cells[y][x].Bottom = true
-// 			}
-// 		}
-// 	}
-
-// 	return maze, nil
-// }
-
 func LoadMaze(filename string) (*Maze, error) {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -405,66 +261,6 @@ func LoadMaze(filename string) (*Maze, error) {
 	return maze, nil
 }
 
-// func (m *Maze) GeneratePerfectMaze() {
-// 	type Edge struct {
-// 		x1, y1, x2, y2 int // Координаты соединяемых клеток
-// 	}
-
-// 	edges := []Edge{}
-// 	for y := 0; y < m.Rows; y++ {
-// 		for x := 0; x < m.Cols; x++ {
-// 			if x < m.Cols-1 {
-// 				edges = append(edges, Edge{x, y, x + 1, y}) // Правое соединение
-// 			}
-// 			if y < m.Rows-1 {
-// 				edges = append(edges, Edge{x, y, x, y + 1}) // Нижнее соединение
-// 			}
-// 		}
-// 	}
-
-// 	// Перемешиваем ребра
-// 	rand.Shuffle(len(edges), func(i, j int) {
-// 		edges[i], edges[j] = edges[j], edges[i]
-// 	})
-
-// 	// Создаем структуру для отслеживания соединенных компонентов
-// 	parent := make([]int, m.Rows*m.Cols)
-// 	for i := range parent {
-// 		parent[i] = i
-// 	}
-
-// 	find := func(x int) int {
-// 		if parent[x] != x {
-// 			parent[x] = find(parent[x])
-// 		}
-// 		return parent[x]
-// 	}
-
-// 	union := func(x, y int) {
-// 		rootX := find(x)
-// 		rootY := find(y)
-// 		if rootX != rootY {
-// 			parent[rootX] = rootY
-// 		}
-// 	}
-
-// 	// Генерация лабиринта
-// 	for _, edge := range edges {
-// 		cell1 := edge.y1*m.Cols + edge.x1
-// 		cell2 := edge.y2*m.Cols + edge.x2
-
-// 		if find(cell1) != find(cell2) {
-// 			// Если клетки не соединены, соединяем их и убираем стену
-// 			union(cell1, cell2)
-// 			if edge.x1 == edge.x2 { // Если соединение вертикальное
-// 				m.Cells[edge.y1][edge.x1].Bottom = false
-// 			} else { // Если соединение горизонтальное
-// 				m.Cells[edge.y1][edge.x1].Right = false
-// 			}
-// 		}
-// 	}
-// }
-
 // SaveMaze сохраняет лабиринт в файл в указанном формате
 func (m *Maze) SaveMaze(filename string) error {
 	file, err := os.Create(filename)
@@ -482,15 +278,18 @@ func (m *Maze) SaveMaze(filename string) error {
 	// Записываем стенки справа
 	for y := 0; y < m.Rows; y++ {
 		for x := 0; x < m.Cols; x++ {
-			if x < m.Cols-1 {
+			if x < m.Cols {
 				if m.Cells[y][x].Right {
 					_, err = fmt.Fprintf(file, "1 ")
 				} else {
 					_, err = fmt.Fprintf(file, "0 ")
 				}
+			} else {
+				// Для последнего элемента в строке добавляем "1", чтобы закрыть строку
+				// _, err = fmt.Fprintf(file, "1")
 			}
 		}
-		_, err = fmt.Fprintln(file, "1") // Стенка справа для последней ячейки
+		_, err = fmt.Fprintln(file) // Переход на новую строку
 		if err != nil {
 			return err
 		}
@@ -505,15 +304,18 @@ func (m *Maze) SaveMaze(filename string) error {
 	// Записываем стенки снизу
 	for y := 0; y < m.Rows; y++ {
 		for x := 0; x < m.Cols; x++ {
-			if x < m.Cols-1 {
+			if x < m.Cols {
 				if m.Cells[y][x].Bottom {
 					_, err = fmt.Fprintf(file, "1 ")
 				} else {
 					_, err = fmt.Fprintf(file, "0 ")
 				}
+			} else {
+				// Для последнего элемента в строке добавляем "0", чтобы закрыть строку
+				// _, err = fmt.Fprintf(file, "0")
 			}
 		}
-		_, err = fmt.Fprintln(file, "0") // Нижняя граница для последней ячейки
+		_, err = fmt.Fprintln(file) // Переход на новую строку
 		if err != nil {
 			return err
 		}
@@ -528,14 +330,6 @@ func main() {
 	flag.Parse()
 
 	game := NewGame(*w, *h)
-	// err := game.LoadMazeFromFile("/Users/calamarp/Desktop/go/Go_Maze/src/assets/maze_test.txt")
-	// if err != nil {
-	// 	log.Fatalf("Failed to load maze from file: %v", err)
-	// }
-	// fmt.Println("Исходная матрица:")
-	// for _, row := range game.maze.Cells {
-	// 	fmt.Println(row)
-	// }
 	// Печатаем сгенерированный лабиринт в терминал
 	fmt.Println("Сгенерированный лабиринт:")
 	game.maze.PrintMaze()
